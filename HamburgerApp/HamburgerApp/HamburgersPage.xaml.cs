fcb28ce3c759504bba49a5d04a1cf6f430b96e5e
brianptrager <brian.trager@rit.edu>
2022-02-16 11:52:24 -0500

    Added ContextActions, Grouping, Events (non-MVVM): ItemTapped, ItemSelected, MenuItem_Clicked, and RefreshCommand
    Next commit will MVVMify this app with Commands

--- HamburgerApp/HamburgerApp/HamburgersPage.xaml.cs
+++ HamburgerApp/HamburgerApp/HamburgersPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HamburgerApp.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,34 @@ namespace HamburgerApp
         public HamburgersPage()
         {
             InitializeComponent();
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            //Let's "null" our selected item here
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var hamburger = ((ListView)sender).SelectedItem as Hamburger;
+            if(hamburger == null)
+            {
+                return;
+            }
+
+            await DisplayAlert("Selected Hamburger", hamburger.Name, "OK");
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var hamburger = ((MenuItem)sender).BindingContext as Hamburger;
+            if(hamburger == null)
+            {
+                return;
+            }
+
+            await DisplayAlert("Favorited Hamburger", hamburger.Name, "OK");
         }
     }
 }
--- HamburgerApp/HamburgerApp/ViewModels/HamburgerViewModel.cs
+++ HamburgerApp/HamburgerApp/ViewModels/HamburgerViewModel.cs
@@ -1,25 +1,42 @@
 ﻿using HamburgerApp.Models;
 using MvvmHelpers;
+using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HamburgerApp.ViewModels
 {
     public class HamburgerViewModel : ViewModelBase
     {
         public ObservableRangeCollection<Hamburger> Hamburgers { get; set; }
-
+        public ObservableRangeCollection<Grouping<string, Hamburger>> HamburgerGroups { get; set; }
+        
+        public AsyncCommand RefreshCommand { get; set; }
         public HamburgerViewModel()
         {
             Title = "Hamburgers";
 
             Hamburgers = new ObservableRangeCollection<Hamburger>();
+            HamburgerGroups = new ObservableRangeCollection<Grouping<string, Hamburger>>();
+
+            RefreshCommand = new AsyncCommand(OnRefresh);
 
             LoadData();
 
             //Hamburgers.Add(new Hamburger() { Name = "Butter Burger", RestuarantName = "Culver's", Image = "hamburger.png" });
         }
+
+        async Task OnRefresh()
+        {
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            IsBusy = false;
+        }
+
 
         private void LoadData()
         {
@@ -27,12 +44,19 @@ namespace HamburgerApp.ViewModels
             var tmpList = new List<Hamburger>()
             {
                 new Hamburger(){Name="Quarter Pounder", RestuarantName="McDonald's", Image=image},
+                new Hamburger(){Name="Big Mac", RestuarantName="McDonald's", Image=image},
                 new Hamburger(){Name="Whopper", RestuarantName="Burger King", Image=image},
+                new Hamburger(){Name="Whopper Jr.", RestuarantName="Burger King", Image=image},
+                new Hamburger(){Name="Stacker King", RestuarantName="Burger King", Image=image},
                 new Hamburger(){Name="Little Cheeseburger", RestuarantName="Five Guys", Image=image},
                 new Hamburger() { Name = "Butter Burger", RestuarantName = "Culver's", Image = image }
             };
 
             Hamburgers.AddRange(tmpList);
+            //System.Threading.Thread.Sleep(500);
+
+            HamburgerGroups.Add(new Grouping<string, Hamburger>("McDonald's", new[] { Hamburgers[0], Hamburgers[1] }));
+            HamburgerGroups.Add(new Grouping<string, Hamburger>("Burger King", new[] { Hamburgers[2], Hamburgers[3], Hamburgers[4] }));
         }
     }
 }
